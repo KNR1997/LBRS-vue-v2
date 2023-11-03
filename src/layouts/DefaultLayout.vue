864c2a65554074b2e1b3294c25f82c538fb18c30
KNR1997 <kethakaranasinghe@gmail.com>
2023-11-03 21:21:53 +0530

    first commit

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <Navbar />
+    <Hero />
+    <Features />
+    <Footer />
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+import Hero from '../components/Hero.vue'
+import Features from '../components/Feature.vue'
+
+import Footer from '../components/Footer.vue'
+</script>
